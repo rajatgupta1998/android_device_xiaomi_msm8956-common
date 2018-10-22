81e365073da74ba1a48f9aaddab3b3ec3116b757
Rk779 <Rahulkrishna585@gmail.com>
2018-10-22 13:41:40 +0000

    msm8956-common: Go back to passthrough camera hidl hal
    * Fixes lags while switching between rear cam and front cam

--- msm8956.mk
+++ msm8956.mk
@@ -125,7 +125,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     camera.msm8952 \
     libqomx_core \
